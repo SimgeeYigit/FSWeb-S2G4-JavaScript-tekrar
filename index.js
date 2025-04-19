2e59b32d1d6cd4e847ae89b37df198ae8f40f9e5
Simge Yiğit <simgeyigit98@gmail.com>
2025-04-19 17:24:17 +0300

    3b çözümü tamamlandı.

--- index.js
+++ index.js
@@ -113,8 +113,11 @@ for (let i = 0; i < sayilar.length;i++) {
 }
 
 // 3b çözümü:
-
-/* kodlar buraya */
+ucetambolunenler = [];
+sayilar.forEach(sayi => {
+  if (sayi % 3 === 0) {
+    ucetambolunenler.push(sayi);
+} })
 
 // 3c çözümü:
 
